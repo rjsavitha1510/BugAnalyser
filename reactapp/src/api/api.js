2b3663bf7a47dbfa2ef186bb393164d4b1ec4ce3
rjsavitha1510 <rjsavitha1510@gmail.com>
2025-09-17 18:47:10 +0530

    Create api.js

---
+++ reactapp/src/api/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const API=axios.create({
+    baseURL:0||'http://localhost:8080/api',
+});
+API.interceptors.request.use((config)=>{
+    const token=localStorage.getItem('token');
+    if(token)config.headers.Authorization=`Bearer ${token}`;
+    return config;
+});
+export default API;
