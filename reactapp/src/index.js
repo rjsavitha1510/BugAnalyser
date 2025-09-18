cbf68742ddefe4296a92a31a798a052cd3312322
rjsavitha1510 <rjsavitha1510@gmail.com>
2025-09-18 18:56:04 +0530

    Create index.js

---
+++ reactapp/src/index.js
@@ -0,0 +1,19 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
