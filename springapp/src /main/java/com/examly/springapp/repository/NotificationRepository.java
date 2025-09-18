a84bf42bcade8c204b1bfaede845d4a12cd37925
rjsavitha1510 <rjsavitha1510@gmail.com>
2025-09-18 19:38:50 +0530

    Create NotificationRepository.java

---
+++ springapp/src /main/java/com/examly/springapp/repository/NotificationRepository.java
@@ -0,0 +1,15 @@
+package com.examly.springapp.repository;
+
+import com.examly.springapp.model.Notification;
+import com.examly.springapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUser(User user);
+
+}
