6344dd4a54a11f2d3ca04de7b7136211827d4343
rjsavitha1510 <rjsavitha1510@gmail.com>
2025-09-18 18:34:20 +0530

    Create AuthContext.js

---
+++ reactapp/src/context/AuthContext.js
@@ -0,0 +1,22 @@
+import { createContext, useState, useContext } from "react";
+const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const login = (token) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => useContext(AuthContext);
