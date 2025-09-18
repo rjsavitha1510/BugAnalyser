dad406cb54891bb9921ac1b0829297383f9ac319
rjsavitha1510 <rjsavitha1510@gmail.com>
2025-09-18 19:41:01 +0530

    Create RevokedTokenRepository.java

---
+++ springapp/src /main/java/com/examly/springapp/repository/RevokedTokenRepository.java
@@ -0,0 +1,33 @@
+package com.examly.springapp.repository;
+
+import com.examly.springapp.model.RevokedToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RevokedTokenRepository extends JpaRepository<RevokedToken, String> {
+
+    default boolean isTokenRevoked(String token) {
+        return existsById(token);
+    }
+
+    default void saveToken(String token) {
+        save(new RevokedToken(token));
+    }
+}package com.examly.springapp.repository;
+
+import com.examly.springapp.model.RevokedToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RevokedTokenRepository extends JpaRepository<RevokedToken, String> {
+
+    default boolean isTokenRevoked(String token) {
+        return existsById(token);
+    }
+
+    default void saveToken(String token) {
+        save(new RevokedToken(token));
+    }
+}
