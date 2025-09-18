c7e7722791be87040ad1122a92c76f68394ce4ce
rjsavitha1510 <rjsavitha1510@gmail.com>
2025-09-18 19:36:18 +0530

    Create User.java

---
+++ springapp/src /main/java/com/examly/springapp/model/User.java
@@ -0,0 +1,48 @@
+package com.examly.springapp.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "userviewdata")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private String passwordHash; 
+
+    public User() {}
+
+    public User(String username, String email, Role role, String passwordHash) {
+        this.username = username;
+        this.email = email;
+        this.role = role;
+        this.passwordHash = passwordHash;
+    }
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public Role getRole() { return role; }
+    public void setRole(Role role) { this.role = role; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+}
